45ef47da83eebba41bb2bbe00a7aac14fe32b53a
Djibril Keita <djibril@TheWorkplacestartup.com>
2014-03-27 12:37:19 -0400

    Added a little helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    base_title = "Zappening, what's happening around you?"
+    if @title.nil?
+      base_title
+    else  
+      "#{base_title} |#{@title}"
+    end
+  end
 end
