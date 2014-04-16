0ab0ab7286b4dea8636c89336a6c7143533d5e7d
Djibril Keita <djibril@TheWorkplacestartup.com>
2014-04-15 22:11:14 -0400

    Added user model validation

---
+++ db/migrate/20140416014130_add_email_uniqueness_ind.rb
@@ -0,0 +1,6 @@
+class AddEmailUniquenessInd < ActiveRecord::Migration
+  def change
+    add_index :users, :email, :unique => true
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415075219) do
+ActiveRecord::Schema.define(version: 20140416014130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20140415075219) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   create_table "widgets", force: true do |t|
     t.string   "name"
